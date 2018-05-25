c373e11e82835650decd821bd0559e6711664a9d
parisa <sahmadloo@student.unimelb.edu.au>
2018-05-25 11:29:19 +1000

    make some changes

--- Untitled.R
+++ Untitled.R
@@ -3,4 +3,4 @@ ggplot(snv_all_sorted_fitering_cosequence_gene, aes(fct_infreq(gene), fill=conse
   theme(axis.text.x=element_text(angle = 90, hjust = 0)) +
   scale_fill_manual(values= rainbow (14))+
   xlab('gene') + 
-  ylab('# consequence')
+  ylab('# driver')
