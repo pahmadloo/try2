c6cc8656a21929bf239ac127c8cb38da5b12cd7a
parisa <sahmadloo@student.unimelb.edu.au>
2018-05-25 11:54:35 +1000

    i add something new

--- Untitled.R
+++ Untitled.R
@@ -4,3 +4,8 @@ ggplot(snv_all_sorted_fitering_cosequence_gene, aes(fct_infreq(gene), fill=conse
   scale_fill_manual(values= rainbow (14))+
   xlab('gene') + 
   ylab('# consequence')
+
+tableplot(all_variants, select = c( sample_id,gene, driver), sortcol=gene)# good
+library(tabplot)
+ggplot(data = I) + 
+  geom_bar(mapping = aes(x = gene, fill = driver))
